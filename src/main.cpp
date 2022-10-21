16b06f69b08a764317abc2571f6b2e6b8e214897
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2022-10-21 14:24:37 +0200

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include <classa.h>
+#include <classb.h>
+
+int main (void)
+{
+  classa testa;
+  classb testb;
+  testa.test_function_a();
+  testb.test_function_b();
+  return 0;
+}
