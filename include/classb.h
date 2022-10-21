33a6e23b9649389b941d2956f6d99ab19e2a8336
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2022-10-21 14:00:41 +0200

    Create classb.h

---
+++ include/classb.h
@@ -0,0 +1,14 @@
+#ifndef CLASSB_H
+#define CLASSB_H
+
+#include "classc.h"
+
+class classb
+{
+  public:
+  void test_function_b(const classc &param);
+  private:
+  classc value_example;
+}
+
+#endif // CLASSB_H
