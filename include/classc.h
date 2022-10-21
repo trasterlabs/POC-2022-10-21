9663c60199f833f05ecc5ee3d28156a254a71326
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2022-10-21 14:19:52 +0200

    Create classc.h

---
+++ include/classc.h
@@ -0,0 +1,17 @@
+#ifndef CLASSC_H
+#define CLASSC_H
+
+class classc
+{
+  public:
+  int a_test_function_for_classa()
+  {
+    return 5;
+  }
+  int a_test_function_for_classb()
+  {
+    return 3;
+  }
+}
+
+#endif // CLASSC_H
