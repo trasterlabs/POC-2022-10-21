526c1799b1e4e266f8980e39e90f900f42b9adcd
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2022-10-23 09:20:27 +0200

    Update classb.cpp

--- src/classb.cpp
+++ src/classb.cpp
@@ -5,5 +5,5 @@
 void classb::test_function_b(const classc &param)
 {
   value_example = param;
-  std::cout << param.a_test_function_for_classb();
+  std::cout << value_example.a_test_function_for_classb();
 }
