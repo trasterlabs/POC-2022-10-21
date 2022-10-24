ca37ec98b559e4b2769e1605e2b16172687be18d
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2022-10-24 09:09:21 +0200

    Update classb.cpp

--- src/classb.cpp
+++ src/classb.cpp
@@ -5,7 +5,9 @@
 void classb::test_function_b(const classc &param)
 {
   value_example = param;
-  std::cout << value_example.a_test_function_for_classb();
+  std::cout << "inside test_function" << std::endl;
+  std::cout << "value a: " << value_example.a_test_function_for_classa() << std::endl;
+  std::cout << "value b: " << value_example.a_test_function_for_classb() << std::endl;
 }
 
 
