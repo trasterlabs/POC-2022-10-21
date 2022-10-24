220a13c04983e4cb6ce353c6063740964774a05e
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2022-10-24 08:55:56 +0200

    Update classa.cpp

--- src/classa.cpp
+++ src/classa.cpp
@@ -1,10 +1,18 @@
 #include "classa.h"
 
 #include <iostream>
+#include <cstdio>
 
 void classa::test_function_a(const classc &param)
 {
   value_example = param;
   std::cout << "inside test_function" << std::endl;
   std::cout << value_example.a_test_function_for_classa() << std::endl;
+}
+
+void classa::function_pointers_show(void)
+{
+  printf("show the function pointers address to show the POC");
+  printf("function 1 pointer: 0x%08x\r\n", (void*)value_example.a_test_function_for_classa);
+  printf("function 2 pointer: 0x%08x\r\n", (void*)value_example.a_test_function_for_classb);
 }
