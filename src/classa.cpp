3a1c8fe99d196eed564576ecbe99554e90c36f35
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2022-10-24 09:11:10 +0200

    Update classa.cpp

--- src/classa.cpp
+++ src/classa.cpp
@@ -13,7 +13,7 @@ void classa::test_function_a(const classc &param)
 
 void classa::function_pointers_show(void)
 {
-  printf("show the function pointers address to show the POC");
+  printf("show the function pointers address to show the POC\n");
   printf("function 1 pointer: 0x%08x\r\n", (void*)&classc::a_test_function_for_classa);
   printf("function 2 pointer: 0x%08x\r\n", (void*)&classc::a_test_function_for_classb);
 }
