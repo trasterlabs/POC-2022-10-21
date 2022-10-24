cbe53649505477242a520087b0dac8e373e515ce
trasterlabs <78925138+trasterlabs@users.noreply.github.com>
2022-10-24 08:06:04 +0200

    Update classa.cpp

--- src/classa.cpp
+++ src/classa.cpp
@@ -6,5 +6,5 @@ void classa::test_function_a(const classc &param)
 {
   value_example = param;
   std::cout << "inside test_function" << std::endl;
-  std::cout << value_example.a_test_function_for_classa(); << std::endl;
+  std::cout << value_example.a_test_function_for_classa() << std::endl;
 }
